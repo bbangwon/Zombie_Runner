3e57774e391c8ade064dd8a70fdd35314e62d622
youngil <youngil@placeb.com>
2024-03-19 09:16:12 +0900

    적의 체력과 데미지

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,7 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;
 
     void Update()
     {
@@ -19,6 +20,13 @@ public class Weapon : MonoBehaviour
         if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log("I hit " + hit.transform.name);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if(target == null) return;
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
         }
     }
 }
