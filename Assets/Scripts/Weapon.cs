75d3912dcb3a73d8a1b1cc8f54b3a024968adff4
youngil <youngil@placeb.com>
2024-03-19 09:40:15 +0900

    사격효과 파티클 추가

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,6 +5,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
 
     void Update()
     {
@@ -16,12 +17,23 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log("I hit " + hit.transform.name);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            if(target == null) return;
+            if (target == null) return;
             target.TakeDamage(damage);
         }
         else
